194a54eab6cf52e968214eec6ac25b8163c8f305
Kiranabrar <kiran.cs577@gmail.com>
2023-09-01 14:44:31 +0500

    update about

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -4,8 +4,8 @@ function About() {
   return (
     <section className="py-10 " id='About'>
     <h2 className="text-3xl font-bold ">About Me</h2>
-    <div className='container mx-auto grid md:grid-cols-2 items-center justify-center md:justify-between ' >
-      <div className=" mt-6 container mx-auto ms-2 px-5">
+    <div className='container mx-auto grid md:grid-cols-2 items-center justify-center px-8 md:justify-between ' >
+      <div className=" mt-6 container mx-auto ">
         <p className=" text-left text-md">
             Hi, My Name is Kiran Abrar. I'm Passionate about creating intuitive user interfaces 
             with a stong focus on clean code and performance optimization.
@@ -13,15 +13,16 @@ function About() {
             Experienced in building responsive and interactive web applications.
             Check out my projects to see my skills in action. 
             Let's collaborate and build something amazing together!<br/>
-            Email: <b>kiran.cs577@gmail.com</b><br/>
-            Contact: <b>+923176834506</b><br/>
-            Address: <b>Burewala,Vehari</b>
+            <button className="bg-[#cd4818] hover:bg-[#671e03] py-2 mt-7 px-4 rounded-md">
+          <a  href="#About">Contact me</a> 
+        </button>
+           
         </p> 
       </div>
       <div className="md:px-11">
         <img
           src={portfolio}
-          className="h-auto max-w-sm mt-4 px-8 max-auto md:ml-auto "
+          className="h-auto w-sm mt-4 px-8 max-auto rounded-md md:ml-auto  "
           alt="..." />
           </div>
           </div>
